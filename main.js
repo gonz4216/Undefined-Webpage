b0012fd5f1e4b008d1c1aeb5e167028ad12cf900
gonz4216 <ethgon19@gmail.com>
2022-02-24 00:20:50 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+//Java script tutorial
+var friends = 10;
+
+//friends = friends + 1;
+//friends = friends - 9;
+//friends = friends * 2;
+//friends = friends / 2;
+//friends = friends ** 2;
+//var remainingFriends = friends % 3;
+
+//friends += 1
+
+//friends -= 1;
+console.log(friends);
